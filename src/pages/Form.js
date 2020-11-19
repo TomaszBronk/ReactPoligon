6177a0c4da1486e1d3e93a8e369195f6e0893550
TomaszB <tomaszbronk11@gmail.com>
2020-11-19 18:02:35 +0100

    bindowanie zdarzen

--- src/pages/Form.js
+++ src/pages/Form.js
@@ -1,6 +1,48 @@
 import React from 'react';
 
+//element klasowy
+class Form extends React.Component {
+    state = {
+        name:''
+    }
+    // bind przypisanie this. setSate
+    // pierwsza metoda 
+   /* constructor(props) {
+        super(props);
+        this.handleChange = this.handleChange.bind(this);
+      //  this.handleClick = this.handleClick.bind(this);
+      //  this.handleSubmit = this.handleSubmit.bind(this);
+    }*/
 
+    handleSubmit(event) {
+        event.preventDefault();
+    }
+    //druga metoda
+    //funkcja strzałkowa
+    handleClick = (event) => {
+        console.log('handleClick: ',event);
+        console.log('React Button to działa');
+    }
+    handleChange = (event) => {
+        //event.target.value
+        console.log('input value: ', event.target.value);
+        this.setState({name: event.target.value});
+        //interpreter js nie wie jaki jest kontekst
+        //setState undefinded
+    }
+    render(){
+        return(
+            <form onSubmit={this.handleSubmit}>  
+                <input type="text" onChange={this.handleChange}/>
+                <button onClick={this.handleClick}>React Button</button>
+            </form>
+        );
+    }
+}
+
+
+//element funkcyjny
+/* 
 function Form() {
 const handleClick = () =>console.log('React Button to działa');
 const handleSubmit= (event) => {
@@ -14,5 +56,5 @@ return(
 );
 
 }
-
+*/
 export default Form;
